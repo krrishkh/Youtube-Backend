a96f880e979a6fdf196481b556d5b35fcb1c88a1
Krrish <krrishk22@iitk.ac.in>
2024-12-26 00:05:03 +0530

    minor updates

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,7 +1,7 @@
 import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import jwt from "jsonwebtoken";
 import { User } from "../models/user.models.js";
+import jwt from "jsonwebtoken";
 
 
 export const verifyJWT = asyncHandler(async(req, res, next) => {
