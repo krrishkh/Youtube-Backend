e0e81b4cb217d2b2870910a180be47370ef1e368
Krrish <krrishk22@iitk.ac.in>
2024-12-25 23:40:05 +0530

    added complex controllers (aggregate pipline) and added routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,18 @@
 import { Router } from "express"
-import { logoutUser, loginUser, registerUser, refreshAccessToken } from "../controllers/user.controllers.js"
+import { 
+    logoutUser, 
+    loginUser, 
+    registerUser, 
+    refreshAccessToken,
+    changeCurrentPassword,
+    updateAccountDetails,
+    currentUser,
+    updateCoverImage,
+    updateAvatar,
+    getUserChannelProfile,
+    getWatchHistory,
+  }
+  from "../controllers/user.controllers.js"
 import { upload } from "../middlewares/multer.middlewares.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js"
 
@@ -25,5 +38,17 @@ router.route("/login").post(loginUser)
 //secured routes 
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, currentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"), updateAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
+
+
+
 
 export default router
